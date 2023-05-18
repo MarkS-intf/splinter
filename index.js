37d1b4a3cbc13be41500b7cfbf81d287b29baf1d
Mark <mark.sapp@interfolio.com>
2023-05-18 15:57:13 -0400

    install eslint using config file from xena., fix all formatting errors

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 require('dotenv').config()
-const { 
+const {
   formatTestRailResponse,
   formatJiraRequest,
   getTestCasesPerSuite,
   createJiraTicket
-} = require('./utils');
+} = require('./utils')
 let formattedTestsForJira = []
 
 async function main() {
   // Error handling
-  if (process.argv.length < 4) {
-    console.error('Missing arguments. Usage: node index.js <sectionId> <jiraEpicId>');
-    process.exit(1);
+  if(process.argv.length < 4) {
+    console.error('Missing arguments. Usage: node index.js <sectionId> <jiraEpicId>')
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
   }
 
   // Setting vars to inputs
-  const sectionId = process.argv[2];
-  const jiraEpicId = process.argv[3];
+  const sectionId = process.argv[2]
+  const jiraEpicId = process.argv[3]
 
   // Getting all test cases under a section, formatting them, and then pushing them to an array
   const apiResponse = await getTestCasesPerSuite(sectionId)
-  
-  for (let i = 0; i < apiResponse.cases.length; i++) {
+
+  for(let i = 0; i < apiResponse.cases.length; i++) {
     const responseData = apiResponse.cases[i]
     const responseTitle = apiResponse.cases[i].title
     const formattedResponse = formatTestRailResponse(responseData)
-    const responseDict = {[responseTitle]: formattedResponse}
+    const responseDict = { [responseTitle]: formattedResponse }
     formattedTestsForJira.push(responseDict)
   }
 
   // For every test object in formattedTestsForJira
-  for (let test of formattedTestsForJira) {
+  for(let test of formattedTestsForJira) {
     // Extract the title and data from the tests
     const title = Object.keys(test)[0]
     const testRailData = Object.values(test)[0]
--- utils.js
+++ utils.js
@@ -3,154 +3,155 @@ const axios = require('axios')
 
 // Initializing credentials for API calls
 const testRailApi = axios.create({
-    baseURL: 'https://interfolio.testrail.com',
-    auth: {
-        username: process.env.TESTRAIL_USERNAME,
-        password: process.env.TESTRAIL_PASSWORD,
-    },
+  baseURL: 'https://interfolio.testrail.com',
+  auth: {
+    username: process.env.TESTRAIL_USERNAME,
+    password: process.env.TESTRAIL_PASSWORD,
+  },
 })
-  
+
 const jiraApi = axios.create({
-    baseURL: 'https://interfolio.atlassian.net',
-    headers: {
-      'Authorization': `Basic ${Buffer.from(
-        process.env.JIRA_USERNAME + ':' + process.env.JIRA_API_KEY
-      ).toString('base64')}`,
-      'Content-Type': 'application/json',
-    },
-  });   
+  baseURL: 'https://interfolio.atlassian.net',
+  headers: {
+    'Authorization': `Basic ${ Buffer.from(
+      process.env.JIRA_USERNAME + ':' + process.env.JIRA_API_KEY
+    ).toString('base64') }`,
+    'Content-Type': 'application/json',
+  },
+})
 
 // Formatting the testrail cases to look nice in Jira
 function formatTestRailResponse(response) {
-    return returnString = 
-    `
-    Testrail: C${response.id}
+  return `
+    Testrail: C${ response.id }
   
-    ${response.custom_description}
+    ${ response.custom_description }
   
     Preconditions:
-    ${response.custom_preconds}
+    ${ response.custom_preconds }
   
     Steps:
-    ${response.custom_steps}
+    ${ response.custom_steps }
   
     Expected Result:
-    ${response.custom_expected}
+    ${ response.custom_expected }
   
     Comments:
-    ${response.custom_review_comments}
+    ${ response.custom_review_comments }
       `
 }
 
 // Formatting jira request body
-function formatJiraRequest(title, epicId, testRailData){
-    const description = `
-    ${testRailData
-      .replace(/\\n/g, '') // Remove '\n' characters
-      .replace(/\\r/g, '') // Remove '\r' characters
-      .replace(/'{2,}/g, '\'') // Remove consecutive single quotes
-      .replace(/\s+\'/g, '\'') // Remove leading spaces before single quotes
-      .replace(/\'\s+/g, '\'') // Remove trailing spaces after single quotes
-      .replace(/(\{|\})/g, '') // Remove curly braces
-      .replace(/"/g, '')
-      .split('\n') // Split the string into an array of lines
-      .map(line => line.trim()) // Trim leading and trailing whitespace from each line
-    }`
-    const trimmedTemplate = description.trim();
-    const formattedTemplate = trimmedTemplate.replace(/\s+/g, ' ');
+function formatJiraRequest(title, epicId, testRailData) {
+  const description = `
+    ${ testRailData
+    .replace(/\\n/g, '') // Remove '\n' characters
+    .replace(/\\r/g, '') // Remove '\r' characters
+    .replace(/'{2,}/g, '\'') // Remove consecutive single quotes
+    .replace(/\s+\'/g, '\'') // Remove leading spaces before single quotes
+    .replace(/\'\s+/g, '\'') // Remove trailing spaces after single quotes
+    .replace(/(\{|\})/g, '') // Remove curly braces
+    .replace(/"/g, '')
+    .split('\n') // Split the string into an array of lines
+    .map(line => line.trim()) // Trim leading and trailing whitespace from each line
+}`
+  const trimmedTemplate = description.trim()
+  const formattedTemplate = trimmedTemplate.replace(/\s+/g, ' ')
 
-    return {
-      "fields": {
-        "project": {
-          "id": "11951"
-        },
-        "issuetype": {
-          "id": "7",
-          "operations": [
-            "set"
-          ]
-        },
-        "summary": `${title}`,
-        "customfield_12171": [],
-        "customfield_10400": `${epicId}`,
-        "customfield_12121": {
-          "id": "11984",
-          "value": "2"
-        },
-        "customfield_12122": {
-          "id": "11988",
-          "value": "2"
-        },
-        "priority": {
-          "id": "3",
-          "name": "Major",
-          "iconUrl": "https://interfolio.atlassian.net/images/icons/priorities/major.svg"
-        },
-        "components": [],
-        "customfield_12085": {
-          "id": "11887",
-          "value": "Team Autobots"
-        },
-        "customfield_12146": [],
-        "description": {
-          "version": 1,
-          "type": "doc",
-          "content": [
-            {
-              "type": "paragraph",
-              "content": [
-                {
-                  "type": "text",
-                  "text": formattedTemplate
-                }
-              ]
-            }
-          ]
-        },
-        "labels": [],
-        "fixVersions": [],
-        "customfield_12011": [
-          {
-            "id": "11700",
-            "value": "Other"
-          }
-        ],
-        "customfield_12141": []
+  return {
+    'fields': {
+      'project': {
+        'id': '11951'
       },
-      "update": {},
-      "watchers": [
-        "61a4dc7cc15977006abcf460"
-      ]
-    }
+      'issuetype': {
+        'id': '7',
+        'operations': [
+          'set'
+        ]
+      },
+      'summary': `${ title }`,
+      'customfield_12171': [],
+      'customfield_10400': `${ epicId }`,
+      'customfield_12121': {
+        'id': '11984',
+        'value': '2'
+      },
+      'customfield_12122': {
+        'id': '11988',
+        'value': '2'
+      },
+      'priority': {
+        'id': '3',
+        'name': 'Major',
+        'iconUrl': 'https://interfolio.atlassian.net/images/icons/priorities/major.svg'
+      },
+      'components': [],
+      'customfield_12085': {
+        'id': '11887',
+        'value': 'Team Autobots'
+      },
+      'customfield_12146': [],
+      'description': {
+        'version': 1,
+        'type': 'doc',
+        'content': [
+          {
+            'type': 'paragraph',
+            'content': [
+              {
+                'type': 'text',
+                'text': formattedTemplate
+              }
+            ]
+          }
+        ]
+      },
+      'labels': [],
+      'fixVersions': [],
+      'customfield_12011': [
+        {
+          'id': '11700',
+          'value': 'Other'
+        }
+      ],
+      'customfield_12141': []
+    },
+    'update': {},
+    'watchers': [
+      '61a4dc7cc15977006abcf460'
+    ]
   }
+}
 
 // Getting all test cases under a suite/section in testrail
 async function getTestCasesPerSuite(suiteId) {
-    try {
-      const response = await testRailApi.get(`index.php?/api/v2/get_cases/1&section_id=${suiteId}`)
-      return response.data
-    } catch (error) {
-      console.error('Error fetching test cases:', error)
-    }
+  try {
+    const response = await testRailApi.get(`index.php?/api/v2/get_cases/1&section_id=${ suiteId }`)
+    return response.data
+  } catch(error) {
+    console.error('Error fetching test cases:', error)
+  }
 }
 
 // Creating a Jira ticket
 async function createJiraTicket(formattedRequest) {
-    try {
-      const response = await jiraApi.post('/rest/api/3/issue', formattedRequest)
-      if (response.status === 201) {
-        console.log(`Jira ticket successfully created with ID: ${response.data.id}`)
-      } else {
-        console.log('Unexpected response status:', response.status)
-      }
-    } catch (error) {
-      console.error('Error creating Jira ticket:', error)
+  try {
+    const response = await jiraApi.post('/rest/api/3/issue', formattedRequest)
+    if(response.status === 201) {
+      // eslint-disable-next-line no-console
+      console.log(`Jira ticket successfully created with ID: ${ response.data.id }`)
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Unexpected response status:', response.status)
     }
-  }  
+  } catch(error) {
+    console.error('Error creating Jira ticket:', error)
+  }
+}
 
-module.exports = { 
-    formatTestRailResponse,
-    formatJiraRequest,
-    getTestCasesPerSuite,
-    createJiraTicket
-};
+module.exports = {
+  formatTestRailResponse,
+  formatJiraRequest,
+  getTestCasesPerSuite,
+  createJiraTicket
+}
